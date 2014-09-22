db58c21be33ffd8079300c52357cf835ebfcd88c
jimomulloy <jimomulloy@gmail.com>
2014-09-22 21:41:30 +0100

    port change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var http = require('http'), express = require('express'), routes = require('./li
 // ------------- END MODULE SCOPE VARIABLES ---------------
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'jshtml');
 app.use(express.favicon());
@@ -39,7 +39,7 @@ routes.configRoutes(app, server);
 // -------------- END SERVER CONFIGURATION ----------------
 
 // ----------------- BEGIN START SERVER -------------------
-server.listen(3000);
+server.listen(4000);
 
 console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 
